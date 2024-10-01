0271136f30c4928c94baec32d4d2942d36ee7107
Maksym Pushkash <maksympushkashwork1@gmail.com>
2024-10-01 19:38:23 +0300

    first commit

---
+++ lab_05.1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+double g(double a, double b); 
+
+int main()
+{
+    double s, t;
+
+    cout << "s = "; cin >> s;
+    cout << "t = "; cin >> t;
+
+    
+    double result = (g(1, s) + (1 + g(t, 1) * g(t, 1)) * (1 + g(t, 1) * g(t, 1))) / (1 + g(s + t, 1) * g(s + t, 1) * g(s + t, 1));
+
+    cout << "Результат: " << result << endl;
+    
+    return 0;
+}
+
+
+double g(double a, double b)
+{
+    return a * a + a * b + b * b;
+}
+
+
